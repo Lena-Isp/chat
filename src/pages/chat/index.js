c3e916900c0e1cb9174c031417c5bb607737e809
Lena-Isp <eispravnikova@gpm-rtv.ru>
2022-02-09 08:01:03 +0300

    adds file

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,4 +1,7 @@
-import {sum} from './modules/sum';
+//стили
+import '../../assets/app.scss';
+
+import {sum} from '../../utils/sum.js';
 
 const root = document.querySelector('#root');
 root.textContent = sum(5, 9).toString();
